7160fed2557e0b52cccaf6c9b8b6691f4168c73f
Ola Andersson <ola.d.andersson@ericsson.com>
2016-10-14 11:31:08 +0200

    Created assignment1

---
+++ assignment1/app.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+
+        $scope.checkFood = function() {
+            var food = $scope.food;
+
+            if (food != undefined && food != "") {
+                var foodItems = food.split(',');
+                var itemCount = 0;
+                foodItems.forEach(function(item) {
+                    if (item != "" && item != " "){ //Do not consider empty items!
+                        itemCount++;
+                    }
+                });
+                if (itemCount < 4) {
+                    $scope.message = "Enjoy!";
+                    $scope.colorClass = "text-success";
+                    $scope.borderClass = "has-success";
+                }
+                else {
+                    $scope.message = "Too much!";
+                    $scope.colorClass = "text-success";
+                    $scope.borderClass = "has-success";
+                }
+            }
+            else{
+                $scope.message = "Please enter data first!";
+                $scope.colorClass = "text-danger";
+                $scope.borderClass = "has-error";
+            }
+        };
+    }
+
+})();
